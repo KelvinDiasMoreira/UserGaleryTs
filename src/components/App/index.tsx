b1fa2203721808aabc05bb590d687adae6233072
kelvin <kelvingremista2015@hotmail.com>
2023-05-17 21:58:58 -0300

    update

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,20 +2,29 @@ import { UserContext } from "../../hooks/UserContext";
 import LoginLabel from "../LoginLabel/index";
 import MainPage from "../MainPage";
 import RegisterComponent from "../RegisterComponent";
+import {Routes, Route} from "react-router-dom"
 
 import { useContext } from "react";
 
 function App() {
-  const { isLogged, registerComponent, registerComplete } =
-    useContext(UserContext);
-
-  if (isLogged || registerComplete) {
-    return <MainPage />;
-  }
-  if (registerComponent) {
-    return <RegisterComponent />;
-  }
-  return <LoginLabel />;
+  return(
+    <Routes>
+      <Route path="/" element={<LoginLabel />}/>
+      <Route path="/register" element={<RegisterComponent />}/>
+      <Route path="/mainpage" element={<MainPage />}/>
+    </Routes>
+  )
 }
 
 export default App;
+
+// const { isLogged, registerComponent, registerComplete } =
+//   useContext(UserContext);
+
+// if (isLogged || registerComplete) {
+//   return <MainPage />;
+// }
+// if (registerComponent) {
+//   return <RegisterComponent />;
+// }
+// return <LoginLabel />;
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App/index.tsx'
 import { GlobalStyles } from './GlobalStyles'
 import { UserContextProvider } from './hooks/UserContext.tsx'
+import { BrowserRouter } from "react-router-dom"
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <BrowserRouter>
     <UserContextProvider>
     <GlobalStyles/>
     <App />
     </UserContextProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
